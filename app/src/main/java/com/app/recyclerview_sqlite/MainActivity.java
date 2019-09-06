0bc60b6d83e3a19e840a92568f8fffc9366940b5
danielrpg <daniel.fernandezgp@gmail.com>
2019-09-05 22:08:51 -0400

    Cursor and Swipe Delete actions

--- app/src/main/java/com/app/recyclerview_sqlite/MainActivity.java
+++ app/src/main/java/com/app/recyclerview_sqlite/MainActivity.java
@@ -1,14 +1,126 @@
 package com.app.recyclerview_sqlite;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SQLiteDatabase mDatabse;
+    private EditText mEditTextName;
+    private TextView mTextViewAmount;
+    private int mAmount = 0;
+    private GroceryAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GroceryDBHelper helper = new GroceryDBHelper(this);
+        mDatabse = helper.getWritableDatabase();
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter = new GroceryAdapter(this, getAllItems());
+        recyclerView.setAdapter(mAdapter);
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,
+                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                removeItem((long) viewHolder.itemView.getTag());
+            }
+        }).attachToRecyclerView(recyclerView);
+
+        mEditTextName = findViewById(R.id.edit_name);
+        mTextViewAmount = findViewById(R.id.textview_amount);
+
+        Button buttonIncrease = findViewById(R.id.button_increase);
+        Button buttonDecrease = findViewById(R.id.button_decrease);
+        Button buttonAdd = findViewById(R.id.button_add);
+
+        buttonIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                increase();
+            }
+        });
+
+        buttonDecrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                decrease();
+            }
+        });
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItem();
+            }
+        });
+    }
+
+    private void increase() {
+        mAmount++;
+        mTextViewAmount.setText(String.valueOf(mAmount));
+    }
+
+    private void decrease () {
+        if (mAmount > 0) {
+            mAmount--;
+            mTextViewAmount.setText(String.valueOf(mAmount));
+        }
+    }
+
+    private void addItem() {
+        if (mEditTextName.getText().toString().trim().length() == 0 || mAmount == 0) {
+            return;
+        }
+
+        String name = mEditTextName.getText().toString();
+        ContentValues values = new ContentValues();
+        values.put(GroceryContract.GroceryEntry.COLUMN_NAME, name);
+        values.put(GroceryContract.GroceryEntry.COLUMN_AMOUNT, mAmount);
+
+        mDatabse.insert(GroceryContract.GroceryEntry.TABLE_NAME, null, values);
+        mAdapter.swapCursor(getAllItems());
+        mEditTextName.getText().clear();
+    }
+
+    private void removeItem(long id) {
+        mDatabse.delete(GroceryContract.GroceryEntry.TABLE_NAME,
+                GroceryContract.GroceryEntry._ID + "=" + id,
+                null);
+        mAdapter.swapCursor(getAllItems());
+    }
+
+    private Cursor getAllItems() {
+        return mDatabse.query(
+                GroceryContract.GroceryEntry.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                GroceryContract.GroceryEntry.COLUMN_TIMESTAMP + " DESC"
+        );
     }
 }
